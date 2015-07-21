081802146fca13458f61115d749824a1838dab74
unknown <rjederks@5-PARA-12-0152.lumcnet.prod.intern>
2015-07-21 09:45:45 +0200

    Strange character removed from equate.R Add function merge_xcms_meta.

---
+++ R/merge_xcms_meta.R
@@ -0,0 +1,25 @@
+#' Merge a XCMS output file with meta data
+#' @description This functions combines meta data with a XCMS output file. The merge is done based on the sample name.
+#' @param XcmsFile the XCMS output file. This is a comma seperated file.
+#' @param MetaFile the meta file. This file should be comma seperated.
+#' @return a data frame with the meta data and the XCMS data combined.
+#' @details This functions is based on our data analysis protocol. The XCMS peak picking is done on the QCpool and actual samples.
+#'   Sample names should start with QCpool_ or Sample_. This is used within the function to find where the actual data starts.
+#' @author Rico Derks
+merge_xcms_meta <- function(XcmsFile, MetaFile) {
+  # read the meta data
+  MetaData <- read.table(file=MetaFile, header=TRUE, sep=",");
+
+  # read the xcms output file
+  XCMSData <- read.table(file=XcmsFile, header=TRUE, sep=",");
+  FeatureNames <- XCMSData[, "name"];
+  DataStart <- grep(x=colnames(XCMSData), pattern="QCpool_")[1];
+  # only get the feature data and transpose the matrix (samples on rows and features as columns)
+  PeakData <- t(XCMSData[, DataStart:ncol(XCMSData)]);
+  colnames(PeakData) <- FeatureNames;
+  SampleNames <- rownames(PeakData);
+
+  # combine metadata with the peakdata
+  # by.y=0 -> 0 means by rownames
+  merge_xcms_meta <- merge(MetaData, PeakData, by.x="SampleName", by.y=0);
+}
