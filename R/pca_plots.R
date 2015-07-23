bcd00cb431675f1b6226be7fabdc2f4842475e15
unknown <rjederks@5-PARA-12-0152.lumcnet.prod.intern>
2015-07-23 11:33:43 +0200

    Scores plot and loadings plot working now with some extra options.

--- R/pca_plots.R
+++ R/pca_plots.R
@@ -10,31 +10,49 @@
 #' @param type Which plot to generate: scores, loadings or summary plot.
 #' @param T2 add a Hotteling T2 ellipse to the scores plot. Default is true.
 #' @param hotelling Set the Hotelling confidence interval. Default = 0.95.
-#' @details Your data frame should contain the variable names \code{pc1} and \code{pc2}.
+#' @param show.groups color the score plot according to the groups. Default is false.
+#' @param group.name the name of the group in the data frame
+#' @details Your data frame should contain the variable names \code{pc1} and
+#'   \code{pc2}. For every extra principal component use \code{pc3}, \code{pc4},
+#'   etc. In the score plot the scores can be colored by group.name. Set show.groups to true.
 #' @author Rico Derks
-pca_plots <- function(data, xPC=1, yPC=2, type=c("scores", "loadings", "summary"), T2=TRUE, hotelling=0.95, show.groups=FALSE, group.names, group.ids, ...) {
+pca_plots <- function(mydata, xPC=1, yPC=2, type=c("scores", "loadings", "summary"), T2=TRUE, hotelling=0.95, show.groups=FALSE, group.name=NULL, ...) {
   type <- match.arg(type);
+  # this is not working?!?!
   if (is.null(type)) {
     type <- "summary";
   }
 
+  # start the plot
+  p <- ggplot2::ggplot();
   switch(type,
          scores = {
-           p <- ggplot();
-           p <- p + geom_point(data=data, aes_string(x=paste("pc", xPC, sep=""), y=paste("pc", yPC, sep="")), size=3);
-           p <- p + geom_hline(aes(yintercept=0), colour="gray");
-           p <- p + geom_vline(aes(xintercept=0), colour="gray");
-           if (T2 == TRUE) {
-             el <- simple_ellipse(x=data[, paste("pc", xPC, sep="")], y=data[, paste("pc", yPC, sep="")], alpha=hotelling);
-             p <- p + geom_path(data=el, aes(x=x, y=y), colour="gray")
+           if (show.groups == FALSE) {
+             p <- p + ggplot2::geom_point(data=mydata, ggplot2::aes_string(x=paste("pc", xPC, sep=""), y=paste("pc", yPC, sep="")), size=3);
+           } else {
+              if (!is.null(group.name)) {
+                p <- p + ggplot2::geom_point(data=mydata, ggplot2::aes_string(x=paste("pc", xPC, sep=""), y=paste("pc", yPC, sep=""), colour=group.name), size=3);
+              } else {
+                stop("No group name specified!");
+              }
            }
+           if (T2 == TRUE) {
+             el <- simple_ellipse(x=mydata[, paste("pc", xPC, sep="")], y=mydata[, paste("pc", yPC, sep="")], alpha=hotelling);
+             p <- p + ggplot2::geom_path(data=el, ggplot2::aes(x=x, y=y), colour="gray")
+           }
+           p <- p + ggplot2::ggtitle("Scores plot");
          },
          loadings = {
-
+           p <- p + ggplot2::geom_point(data=mydata, ggplot2::aes_string(x=paste("pc", xPC, sep=""), y=paste("pc", yPC, sep="")), size=3);
+           p <- p + ggplot2::ggtitle("Loadings plot");
          },
          summary = {
 
          }
   );
+  p <- p + ggplot2::geom_hline(ggplot2::aes(yintercept=0), colour="gray");
+  p <- p + ggplot2::geom_vline(ggplot2::aes(xintercept=0), colour="gray");
+  p <- p + ggplot2::xlab(paste("PC", xPC, sep=" "));
+  p <- p + ggplot2::ylab(paste("PC", yPC, sep=" "));
   return(p);
 }
