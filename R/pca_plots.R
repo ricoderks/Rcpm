a834d9a05e6156dc4375a4af2bc01914f40b3e8b
Rico Derks <r.j.e.derks@lumc.nl>
2015-08-03 15:27:53 +0200

    Changed pca_plots so that it can handle shapes and colours properly. Last fix didn't work good enough. Version goes up to v0.5.

--- R/pca_plots.R
+++ R/pca_plots.R
@@ -3,21 +3,29 @@
 #'   (includings Hotelling T2 confidence interval ellipse), loading plot or a
 #'   summary plot. The summary plot contains a score plot, loadings plot and the
 #'   summary of fit plot.
-#' @param mydata Dataframe containing all information. See details for more
+#' @param data Dataframe containing all information. See details for more
 #'   information on the structure.
-#' @param xPC Principal component for the x-axis.
-#' @param yPC Principal component for the y-axis.
+#' @param x Principal component for the x-axis.
+#' @param y Principal component for the y-axis.
 #' @param type Which plot to generate: scores, loadings or summary plot.
 #' @param T2 add a Hotteling T2 ellipse to the scores plot. Default is true.
 #' @param hotelling Set the Hotelling confidence interval. Default = 0.95.
-#' @param show.groups color the score plot according to the groups. Default is false.
-#' @param group.name the name of the group in the data frame.
+#' @param colour Set the color of the points for different groups.
+#' @param shape Set the shape of the points for different groups.
 #' @return A ggplot2 plot is returned.
-#' @details Your data frame should contain the variable names \code{PC1} and
-#'   \code{PC2}. For every extra principal component use \code{PC3}, \code{PC4},
-#'   etc. In the score plot the scores can be colored by group.name. Set show.groups to true.
-#' @author Rico Derks
-pca_plots <- function(mydata, xPC=1, yPC=2, type=c("scores", "loadings", "summary"), T2=TRUE, hotelling=0.95, show.groups=FALSE, group.name=NULL, ...) {
+#' @details \code{data} should be data frame which contains at least 2 variables
+#'   for plotting the scores or loadings plot. The name of these 2 variables are
+#'   also used as label for the axes.
+#'
+#'   More variables can be added for coloring
+#'   or shaping the points. These variables should be factors. See the examples
+#'   for a simple example.
+#' @examples
+#' # create a dummy frame
+#' mydata <- data.frame(pc1=rnorm(40), pc2=rnorm(40), groups=as.factor(c(rep(1, 20), rep(2, 20))), shapes=as.factor(c(rep(1, 20),rep(2, 20))));
+#' p <- pca_plots(data=mydata, x="pc1", y="pc2", type="scores", colour="groups", shape="shapes");
+pca_plots <- function(data, x, y, type=c("scores", "loadings", "summary"), T2=TRUE, hotelling=0.95, colour=NULL, shape=NULL) {
+
   type <- match.arg(type);
 
   # start the plot
@@ -25,22 +33,14 @@ pca_plots <- function(mydata, xPC=1, yPC=2, type=c("scores", "loadings", "summar
   switch(type,
          scores = {
            if (T2 == TRUE) {
-             el <- simple_ellipse(x=mydata[, paste("PC", xPC, sep="")], y=mydata[, paste("PC", yPC, sep="")], alpha=hotelling);
-             p <- p + ggplot2::geom_path(data=el, ggplot2::aes(x=x, y=y), colour="gray")
+             el <- simple_ellipse(x=data[, x], y=data[, y], alpha=hotelling);
+             p <- p + ggplot2::geom_path(data=el, ggplot2::aes(x=x, y=y), colour="gray");
            }
-           if (show.groups == FALSE) {
-             p <- p + ggplot2::geom_point(data=mydata, ggplot2::aes_string(x=paste("PC", xPC, sep=""), y=paste("PC", yPC, sep="")), size=3);
-           } else {
-              if (!is.null(group.name)) {
-                p <- p + ggplot2::geom_point(data=mydata, ggplot2::aes_string(x=paste("PC", xPC, sep=""), y=paste("PC", yPC, sep=""), colour=group.name), size=3);
-              } else {
-                stop("No group name specified!");
-              }
-           }
+           p <- p + ggplot2::geom_point(data=data, ggplot2::aes_string(x=x, y=y, colour=colour, shape=shape), size=3);
            p <- p + ggplot2::ggtitle("Scores plot");
          },
          loadings = {
-           p <- p + ggplot2::geom_point(data=mydata, ggplot2::aes_string(x=paste("PC", xPC, sep=""), y=paste("PC", yPC, sep="")), size=3);
+           p <- p + ggplot2::geom_point(data=data, ggplot2::aes_string(x=x, y=y), size=3);
            p <- p + ggplot2::ggtitle("Loadings plot");
          },
          summary = {
@@ -49,7 +49,7 @@ pca_plots <- function(mydata, xPC=1, yPC=2, type=c("scores", "loadings", "summar
   );
   p <- p + ggplot2::geom_hline(ggplot2::aes(yintercept=0), colour="gray");
   p <- p + ggplot2::geom_vline(ggplot2::aes(xintercept=0), colour="gray");
-  p <- p + ggplot2::xlab(paste("PC", xPC, sep=" "));
-  p <- p + ggplot2::ylab(paste("PC", yPC, sep=" "));
+  p <- p + ggplot2::xlab(x);
+  p <- p + ggplot2::ylab(y);
   return(p);
 }
