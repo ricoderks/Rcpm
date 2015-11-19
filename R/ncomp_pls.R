65bc42a3be419e7606aec521ebca03051282399f
Rico Derks <r.j.e.derks@lumc.nl>
2015-11-19 15:32:46 +0100

    opls and o2pls code added

---
+++ R/ncomp_pls.R
@@ -0,0 +1,72 @@
+ncomp_opls <- function(Xi, Yi, prep, nrcv){
+### Find the optimal number of OPLS components based on the highest Q2 value
+### INPUT
+#	Xi			=	X matrix N samples x K variables
+#	Yi			=	Y matrix N samples x I variables (currently works only with single Y)
+#	prep		=	preprocessing ("uv" for UV scaling, "mc" for mean centering, "no" for no preprocessing)
+#	nrcv		=	number of folds in cross-validation
+###	OUTPUT
+#	List of three elements: 
+#		$A 		=	number of correlated components
+#		$ncox	=	number of orthogonal components in X
+#		$ncoy	=	number of orthogonal components in Y
+	#### Estimate number of components for O2PLS 
+	if (prep == "no") {
+		X <- Xi;
+		Y <- Yi;
+	}
+	
+	if (prep == "mc") {
+		X <- scale(Xi, scale=FALSE);
+		Y <- scale(Yi, scale=FALSE);
+	}	
+	
+	if (prep == "uv") {
+		X <- scale(Xi);
+		Y <- scale(Yi);
+	}	
+	
+	#else stop("unknown preprocessing")
+#x <- t(Y)%*%X
+#y <- t(X)
+	
+	
+#CV <- t(Y)%*%X
+#A <- qr(CV)$rank
+#A <-1
+	### Estimate number of Y-orthogonal components by nrcv-fold CV
+	Q2 <- opls(X, Y, prep, 1, 0, 0, nrcv)$Q2Yhatcum;
+	
+	A <- 1;
+	if (ncol(Yi) > 1){
+		for (i in 1:(ncol(Xi) - 1)) {
+			Q2 <- c(Q2, opls(X,Y, prep, i+1, 0, 0, nrcv)$Q2Yhatcum);
+			if (Q2[i+1] < Q2[i]) break
+		}
+		A <- i - 1;
+	}
+	
+	Q2 <- opls(X, Y, prep, A, 0, 0, nrcv)$Q2Yhatcum;
+	
+	for (i in 1:ncol(Xi)) {
+		Q2 <- c(Q2, opls(X, Y, prep, A, i, 0, nrcv)$Q2Yhatcum);
+		if (Q2[i+1] < Q2[i]) break
+	}
+	ncox <- i - 1;
+	
+	### Estimate number of X-orthogonal components by nrcv-fold CV
+	Q2 <- opls(X, Y, prep, A, ncox, 0, nrcv)$Q2Xhatcum;
+	ncoy <- 0;
+	
+	if (ncol(Yi) > 1) {
+		for (i in 1:ncol(Yi)) {
+			Q2 <- c(Q2, opls(X, Y, prep, A, ncox, i, nrcv)$Q2Xhatcum);
+			if (Q2[i+1] < Q2[i]) break
+		}
+		ncoy <- i - 1;
+	}
+	
+	rr <- list(A, ncox, ncoy);
+	names(rr) <- c("A", "ncox", "ncoy");
+	return(rr);
+}
---
+++ R/opls_pred.R
@@ -0,0 +1,55 @@
+opls_pred <- function(A, model, direction) {
+	
+# Prediction function of the o2pls package
+#
+# Input:
+#   A: Observations
+#   model: o2pls model
+#   direction: from x to y ('xy') or from y to x ('yx')
+# Output
+#   Bhat : Predictions
+#   Scorenew : Corresponding scores
+	
+	modelPred <- c();
+	
+	if (direction == "xy") {
+		To <- c();
+		
+		if (length(model$TYosc) != 0) {
+			for (i in 1:ncol(model$TYosc)) {
+				to <- A %*% model$Wosc[ , i] %*% solve((t(model$Wosc[ , i]) %*% model$Wosc[ , i]));
+				To <- cbind(To, to);
+				A <- A - to %*% t(model$PYosc[ , i]);
+			}
+		}
+		
+		T <- A %*% model$svd$u %*% solve((t(model$svd$u) %*% model$svd$u));
+		Yhat <- T %*% model$Bt %*% t(model$svd$v);
+		
+		modelPred$T <- T;
+		modelPred$Yhat <- Yhat;
+		if (length(model$TYosc) != 0) modelPred$To <- To;
+	}
+	
+	if (direction == "yx"){
+		Uo <- c();
+		
+		if (length(model$UXosc) !=0 ) {
+			for (i in 1:ncol(model$UXosc)) { 
+				uo <- A %*% model$Cosc[ , i] %*% solve((t(model$Cosc[ , i]) %*% model$Cosc[ , i]));
+				Uo <- cbind(Uo, uo); 
+				A <- A-uo %*% t(model$PXosc[ , i]);
+			}
+		}
+		
+		U <- A %*% model$svd$v %*% solve((t(model$svd$v) %*% model$svd$v));
+		Xhat <- U %*% model$Bu %*% t(model$svd$u);
+		modelPred$U <- U;
+		modelPred$Xhat <- Xhat;
+		if (length(model$UXosc) != 0) {
+			modelPred$Uo <- Uo;
+		}
+	}		
+	
+	return(modelPred)
+}
