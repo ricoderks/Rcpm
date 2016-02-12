cb158a5038c55ccce2229ecea568fc17300e45e0
rjederks <r.j.e.derks@lumc.nl>
2016-02-12 08:46:44 +0100

    Added function m_pow_x

---
+++ R/m_pow_x.R
@@ -0,0 +1,11 @@
+m_pow_x <- function(data, pca_model) {
+	# calculate initial standard deviation of each variable
+	SV0k <- apply(data, 2, sd);
+	# get the residuals of the PCA model
+	res_pca <- residuals(pca_model);
+	# calculate the standard deviation of the residuals of the PCA model 
+	SVk <- apply(res_pca, 2, sd);
+	# calculate modeling power of the variable
+	MpowX <- 1 - SVk / SV0k;
+	return(MpowX);
+}
