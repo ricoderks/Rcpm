faa159dcd73d614fc5d09a37b6523856a92153ca
rjederks <r.j.e.derks@lumc.nl>
2016-03-02 13:12:34 +0100

    Is NA was present in the data m_pow_x returned NA's. Now NA's are removed.

--- R/m_pow_x.R
+++ R/m_pow_x.R
@@ -1,6 +1,6 @@
 m_pow_x <- function(data, pca_model) {
 	# calculate initial standard deviation of each variable
-	SV0k <- apply(data, 2, sd);
+	SV0k <- apply(data, 2, function(x) sd(x, na.rm=TRUE));
 	# get the residuals of the PCA model
 	res_pca <- residuals(pca_model);
 	# calculate the standard deviation of the residuals of the PCA model 
