90cc43db4f95748ba8b442d9a5ef2fd7c31e3b90
rjederks <r.j.e.derks@lumc.nl>
2016-04-05 14:23:29 +0200

    Added check_alignment function to check if there are problems with the retention time in aligned mzxml files.

---
+++ R/check_alignment.R
@@ -0,0 +1,24 @@
+check_alignment <- function(files) {
+	# sanity check
+	if ((is.vector(files) && is.list(files)) && !is.character(files)) {
+		stop("files is not a character vector!!")
+	}
+	# check if files exist
+	if (!all(file.exists(files))) {
+		stop("File doesn't exist!")
+	}
+	# assume all is fine
+	all_fine <- TRUE
+	cat("Checking....")
+	for (a in 1:length(files)) {
+		tmp <- read_xml(files[a])
+		all_rt <- xml_text(xml_find_all(tmp, "//@retentionTime"))
+		all_rt_num <- as.numeric(regmatches(m=regexpr(pattern="[0-9]{1,3}.[0-9]{1,7}", text=all_rt), x=all_rt))
+		if (length(which(diff(all_rt_num) >= 0)) < (length(all_rt_num) - 1)) {
+			cat("Something wrong with : ", files[a], "\n"); flush.console()
+			all_fine <- FALSE
+		}
+	}
+	cat("....done\n")
+	return(all_fine)
+}
