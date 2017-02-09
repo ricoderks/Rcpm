cb8c23d37de6b362cbd31dcd5814b23a8b6300a1
Rico Derks <r.j.e.derks@lumc.nl>
2017-02-09 11:05:50 +0100

    Start of writing the targeted analysis with xcms vignette.

---
+++ R/Rcpm.R
@@ -0,0 +1,11 @@
+#' @title General functions for the Center of Proteomics and Metabolomics (LUMC)
+#' 
+#' @description
+#' This package contains some general functions for use within the Center of 
+#' Proteomics and Metabolomics
+#' 
+#' @details 
+#' It contains all kind of functions. Most of them still need examples.
+#' 
+#' A vignette is written for targeted analysis with xcms.
+"_PACKAGE"
