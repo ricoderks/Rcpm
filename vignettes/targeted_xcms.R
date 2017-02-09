450984a13a93aeeb42d74ccc8292179bd1ee4db7
Rico Derks <r.j.e.derks@lumc.nl>
2017-02-09 12:49:45 +0100

    update of change log file and last changes to vignette output files

--- vignettes/targeted_xcms.R
+++ vignettes/targeted_xcms.R
@@ -2,8 +2,7 @@
 #  library(dplyr)
 #  library(xcms)
 #  library(Rcpm)
-#  WorkDir <- "/home/rjederks/Documents/Projects/TargetedXCMS/"
-#  DataDir <- paste0(WorkDir, "Data")
+#  library(purrr)
 
 ## ----process_settings, eval=FALSE----------------------------------------
 #  settings <- list()
@@ -35,5 +34,36 @@
 #                           full.names = TRUE)
 #  
 #  raw_data <- xcmsraw_to_tbl(files = list_mzxml,
-#                             profparam = settings$xcmsRaw)
+#                             profparam = settings$xcmsRaw$profparam)
+
+## ----merge_raw_standards, eval=FALSE-------------------------------------
+#  data <- target_peaks %>%
+#    mutate(rt = rt * 60) %>%
+#    list %>%
+#    rep(nrow(raw_data)) %>%
+#    data_frame(stds = .) %>%
+#    bind_cols(raw_data, .)
+#  
+#  # remove raw data datatable
+#  rm(raw_data)
+
+## ----add_roi, eval=FALSE-------------------------------------------------
+#  data <- data %>%
+#    mutate(ROI = map2(raw, stds, ~ tbl2roi(tbl = .y,
+#                                           raw = .x,
+#                                           ppm = settings$std_match$ppm,
+#                                           rt_tol = settings$std_match$rt_tol)))
+
+## ----detect_peaks, eval=FALSE--------------------------------------------
+#  findPeaks_l <- lift_dl(findPeaks) # trick to make findPeaks accept a list of arguments.
+#  
+#  # run it
+#  data <- data %>%
+#    mutate(peakTable = map2(.x = raw,
+#                            .y = ROI,
+#                            .f = ~ findPeaks_l(settings$findPeaks,
+#                                               object = .x,
+#                                               ROI.list = .y) %>%
+#                              as.data.frame %>%
+#                              as.tbl))
 
