a7e170fd73a03588277e0bd20e41089002e1446c
Rico Derks <r.j.e.derks@lumc.nl>
2020-06-04 12:19:20 +0200

    Using tidyeval for pca_plots.

--- R/pca_plots.R
+++ R/pca_plots.R
@@ -5,7 +5,7 @@
 #' summary plot. The summary plot contains a score plot, loadings plot and the 
 #' summary of fit plot.
 #' 
-#' @param data Dataframe containing all information. See details for more information on the structure.
+#' @param data dataframe containing all information. See details for more information on the structure.
 #' @param x Principal component for the x-axis.
 #' @param y Principal component for the y-axis.
 #' @param type Which plot to generate: scores, loadings or summary plot.
@@ -24,21 +24,32 @@
 #' for a simple example.
 #' 
 #' @export
-#' @importFrom ggplot2 ggplot aes aes_string
+#' @importFrom ggplot2 ggplot aes geom_point geom_polygon labs ggtitle geom_hline geom_vline
+#' @importFrom rlang enquo !! as_label
+#' @importFrom methods is
 #'
 #' @author Rico Derks
 #' @examples
 #' # create a dummy frame 
-#' mydata <- data.frame(pc1 = rnorm(40),
-#'                      pc2 = rnorm(40),
+#' mydata <- data.frame(PC1 = rnorm(40),
+#'                      PC2 = rnorm(40),
 #'                      groups = as.factor(c(rep(1, 20), rep(2, 20))),
 #'                      shapes = as.factor(c(rep(1, 20), rep(2, 20))))
 #' p <- pca_plots(data = mydata, 
-#'                x = "pc1",
-#'                y = "pc2",
+#'                x = PC1,
+#'                y = PC2,
 #'                type = "scores",
-#'                colour = "groups",
-#'                shape = "shapes")
+#'                colour = groups,
+#'                shape = shapes)
+#' # or
+#' library(dplyr)
+#' mydata %>%
+#'   pca_plots(x = PC1,
+#'             y = PC2,
+#'             type = "scores",
+#'             colour = groups,
+#'             shape = shapes)
+
 pca_plots <- function(data, 
                       x, 
                       y, 
@@ -48,30 +59,79 @@ pca_plots <- function(data,
                       colour = NULL, 
                       shape = NULL, 
                       size = 3) {
-
+  ## Some sanity checks
+  # check if correct type is selected
   type <- match.arg(type)
+  ## check if data is a data frame
+  if (!is(data, "data.frame")) {
+    stop("'data' does not appear to be a data frame!")
+  }
+  # is x a column in the dataframe
+  if (!deparse(substitute(x)) %in% names(data)) {
+    stop(paste0("'", deparse(substitute(x)), "' is not the name of a variable in '", deparse(substitute(data)),"'"))
+  }
+  # is y a column in the dataframe
+  if (!deparse(substitute(y)) %in% names(data)) {
+    stop(paste0("'", deparse(substitute(y)), "' is not the name of a variable in '", deparse(substitute(data)), "'"))
+  }
+  # is colour a column in the dataframe
+  if (!deparse(substitute(colour)) %in% names(data)) {
+    stop(paste0("'", deparse(substitute(colour)), "' is not the name of a variable in '", deparse(substitute(data)), "'"))
+  }
+  # is shape a column in the dataframe
+  if (!deparse(substitute(shape)) %in% names(data)) {
+    stop(paste0("'", deparse(substitute(shape)), "' is not the name of a variable in '", deparse(substitute(data)), "'"))
+  }
 
+  x <- enquo(x)
+  y <- enquo(y)
+  colour <- enquo(colour)
+  shape <- enquo(shape)
+  
   # start the plot
   p <- ggplot()
   switch(type,
          scores = {
            if (T2 == TRUE) {
-             p <- p + geom_polygon(data = simple_ellipse(x = data[, x], y = data[, y], alpha = hotelling), aes(x = x, y = y), colour = "gray", fill = "white", alpha = 0.5)
+             p <- p + geom_polygon(data = simple_ellipse(x = data[, as_label(x)], 
+                                                         y = data[, as_label(y)],
+                                                         alpha = hotelling), 
+                                   aes(x = x, 
+                                       y = y), 
+                                   colour = "gray", 
+                                   fill = "white", 
+                                   alpha = 0.5)
            }
-           p <- p + geom_point(data = data, aes_string(x = x, y = y, colour = colour, shape = shape), size = size)
-           p <- p + ggtitle("Scores plot")
+           p <- p + 
+             geom_point(data = data, 
+                        aes(x = !!x, 
+                            y = !!y, 
+                            colour = !!colour, 
+                            shape = !!shape), 
+                        size = size)
+           p <- p + 
+             ggtitle("Scores plot")
          },
          loadings = {
-           p <- p + geom_point(data = data, aes_string(x = x, y = y), size = size)
-           p <- p + ggtitle("Loadings plot")
+           p <- p + 
+             geom_point(data = data, 
+                               aes_string(x = x, 
+                                          y = y), 
+                               size = size)
+           p <- p + 
+             ggtitle("Loadings plot")
          },
          summary = {
 
          }
   )
-  p <- p + geom_hline(aes(yintercept = 0), colour = "gray")
-  p <- p + geom_vline(aes(xintercept = 0), colour = "gray")
-  p <- p + xlab(x)
-  p <- p + ylab(y)
+  p <- p + 
+    geom_hline(aes(yintercept = 0), 
+               colour = "gray") + 
+    geom_vline(aes(xintercept = 0), 
+               colour = "gray") + 
+    labs(x = x, 
+         y = y)
+  
   return(p)
 }
