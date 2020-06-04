286e7551167d2192202e371006a7a8a4cc916b0d
Rico Derks <r.j.e.derks@lumc.nl>
2020-06-04 16:10:46 +0200

    Some typo's.

--- R/pqn.R
+++ R/pqn.R
@@ -21,37 +21,37 @@
 #' @references Dieterle, F., Ross, A., Schlotterbeck, G. & Senn, H. Probabilistic Quotient 
 #' Normalization as Robust Method to Account for Dilution of Complex Biological Mixtures. 
 #' Application in H1 NMR Metabonomics. Anal. Chem. 78, 4281-4290 (2006).
-pqn <- function(X, n = "median", QC) {
+pqn <- function(X, n = "median", QC = NULL) {
   X.norm <- matrix(nrow = nrow(X), ncol = ncol(X))
 	colnames(X.norm) <- colnames(X)
 	rownames(X.norm) <- rownames(X)
 
-	if (exists("QC")) {
+	if (!is.null(QC)) {
 		# if QC vector exists, use this as reference spectrum
 		if (length(QC) == 1) {
-			# only 1 reference spectrum given
+			# only 1 reference sample given
 			mX <- as.numeric(X[QC, ])
 		} else {
 			if (n == "mean") {
 				mX <- as.numeric(colMeans(X[QC, ]))
 			}
 			if (n == "median") {
-				mX <- as.numeric(apply(X[QC, ], 2, stats::median))
+				mX <- as.numeric(apply(X[QC, ], 2, median))
 			}
 		}
 	} else {
-		# otherwise use the mean or median of all spectra as referece spectrum
+		# otherwise use the mean or median of all samples as reference sample
 		if (n == "mean") {
 			mX <- as.numeric(colMeans(X))
 		}
 		if (n == "median") {
-			mX <- as.numeric(apply(X, 2, stats::median))
+			mX <- as.numeric(apply(X, 2, median))
 		}
 	}
 
 	# do the actual normalisation
 	for (a in 1:nrow(X)) {
-		X.norm[a,] <- as.numeric(X[a, ] / stats::median(as.numeric(X[a, ] / mX)))
+		X.norm[a, ] <- as.numeric(X[a, ] / median(as.numeric(X[a, ] / mX)))
 	}
 
 	return(X.norm)
