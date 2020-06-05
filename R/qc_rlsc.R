5f2c13182c62f72dff13db76aadeddb32d783c16
Rico Derks <r.j.e.derks@lumc.nl>
2020-06-05 10:19:35 +0200

    qc_rlsc: code cleanup and added verbose parameter (default = FALSE).

--- R/qc_rlsc.R
+++ R/qc_rlsc.R
@@ -2,9 +2,10 @@
 #' 
 #' @description Perform QC-RLSC for batch correction of chromatographic signal.
 #'
-#' @param tab table N*K (row * column) with N samples and K variables
-#' @param colv vector N of numbers: 1 for QC samples and 2 for other samples
-#' @param or ector of measuring order (see details)
+#' @param tab table N*K (row * column) with N samples and K variables.
+#' @param colv vector N of numbers: 1 for QC samples and 2 for other samples.
+#' @param or vector of measuring order (see details).
+#' @param verbose print which variable has been corrected to monitor the process (default = FALSE).
 #'
 #' @return corrected table N*K
 #' 
@@ -16,23 +17,30 @@
 #' @author E. Nevedomskaya
 #' @author Rico Derks
 #' @references Dunn et al. Nature Protocols 6, 1060-1083 (2011)
-qc_rlsc <- function(tab, colv, or) {
-	# create table of the same sizeas initial
+qc_rlsc <- function(tab, colv, or, verbose = FALSE) {
+	# create table of the same size as initial
 	tab_corr <- tab
+	
 	# For each variable (columns) in the initial table
 	for (i in 1:ncol(tab)) {
 		# fit loess curve to the QCs
-		ll <- stats::loess(tab[which(colv == 1), i] ~ or[which(colv == 1)])
+		ll <- loess(tab[which(colv == 1), i] ~ or[which(colv == 1)])
 		
 		# approximate the curve for all the samples
-		aa <- stats::approx(x = or[which(colv == 1)], y = ll$fitted, xout = or)
+		aa <- approx(x = or[which(colv == 1)],
+		             y = ll$fitted, 
+		             xout = or)
 		
 		# correct the variable according to the curve for all the samples
-		tab_corr[,i] <- tab[,i] / aa$y
+		tab_corr[, i] <- tab[, i] / aa$y
 		
 		# print which variable has been corrected in order to monitor the progress
-		#print(i)
+		if(verbose == TRUE) {
+		  print(i)  
+		}
+		
 	}
+	
 	return(tab_corr)
 }
 
